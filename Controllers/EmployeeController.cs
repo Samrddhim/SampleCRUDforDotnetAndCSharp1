deb08af2718ad2d3ac0f2165d9503e48ef278b54
samrddhim <samrddhi.m@tvsmotor.com>
2024-10-29 00:42:28 +0530

    done

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleCRUDusingDotnet.Data;
+using SampleCRUDusingDotnet.Models;
+using SampleCRUDusingDotnet.Models.Entities;
+
+namespace SampleCRUDusingDotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employeeList = dbContext.Employees.ToList();
+            return Ok(employeeList);
+
+        }
+
+        [HttpPost]
+
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Email = addEmployeeDto.Email,
+                Name = addEmployeeDto.Name,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.salary
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+
+        }
+
+        [HttpGet]
+        [Route("{Id:guid}")]
+        public IActionResult getEmployeeById(Guid Id)
+        {
+            var employee = dbContext.Employees.Find(Id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult updateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = dbContext.Employees.Find(id);
+            
+            if(employee is null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+
+        public IActionResult deleteEmployeeById(Guid id)
+        {
+            var employee =dbContext.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            dbContext.Remove(employee);
+            dbContext.SaveChanges();
+
+            return Ok();
+
+        }
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleCRUDusingDotnet.Models.Entities;
+
+namespace SampleCRUDusingDotnet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
